bc1303394f8975f451ed4f7950c9d590dc6e4bea
Ulukkek <hofutop@gmail.com>
2022-03-05 17:48:33 +0600

    feat: new tasks 01-03 and 03-03

---
+++ 01-03/script.js
@@ -0,0 +1,22 @@
+/*let userSelaries={
+    Ella:1200,
+    Sophia:900,
+    Ellia:1000,
+}
+alert(`Зарплата Эллы =${userSelaries.Ella}!`)
+alert(`Зарплата Софии =${userSelaries.Sophia}!`)*/
+
+/*
+let math = {
+    a: 5,
+    b: 6,
+    c: 10,
+}
+alert(`Вот = ${math.a + math.b + math.c}`)
+*/
+
+
+let data = false;
+let number;
+number = (data === true) ? number = 3 : number = 5;
+alert(number)
---
+++ 03-03/script.js
@@ -0,0 +1,14 @@
+/*
+let arr = ['a', 'b', 'c', 'd'];
+alert(`${arr[0]+arr[1]}    ${arr[2]+arr[3]}`)
+*/
+
+/*
+let arr = [10, 15, 9, 10];
+let result=arr[0]*arr[1]+arr[2]*arr[3];
+alert(result);
+*/
+
+let arr = ['a', 'b', 'c'];
+arr.push(1, 2, 3);
+console.log(arr);
