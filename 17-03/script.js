e280512876ba912a520b237c9685ba16c13f7401
Ulukkek <hofutop@gmail.com>
2022-03-19 19:00:43 +0600

    feat: new tasks 17-03

---
+++ 17-03/script.js
@@ -0,0 +1,45 @@
+/*
+let arr = [1, 2, 3, 4, 5, 6];
+let result = arr.reduce(function (sum, elem) {
+    return sum + elem;
+}, 0);
+console.log(result)
+*/
+
+/*
+let arr=[-5,10,5,55,-1,22,-4,-45];
+let result=arr.reduce(function (sum,elem) {
+if(elem<=0){
+    return sum+elem;
+}else{
+    return sum;
+}
+});
+console.log(result);
+*/
+
+
+/******** for each *********/
+
+/*
+let arr = [1, 2, 3, 4, 5, 6];
+let sum;
+arr.forEach(function (item, index, arr) {
+    sum = sum + item;
+})
+console.log(sum)
+*/
+
+let number, elem;
+number = prompt();
+elem = prompt();
+
+let myFunc=(num,element)=>{
+    let arr=[];
+    for (let i = 0; i < num ; i++) {
+        arr.push(element);
+    }
+    console.log(arr);
+}
+
+myFunc(number,elem);
