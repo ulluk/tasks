9b34ee5b539c02c3b97d982eb6e2b4728cee9738
Ulukkek <hofutop@gmail.com>
2022-03-22 20:19:58 +0600

    feat: fixed task number 2 , add "return" in function

--- 22-03/script.js
+++ 22-03/script.js
@@ -1,3 +1,4 @@
+/*
 let a = 4000;
 
 for (let i = 4000; i <= 12000 ; i+=8000) {
@@ -5,16 +6,17 @@ for (let i = 4000; i <= 12000 ; i+=8000) {
         console.log(`Hello after ${i/1000} seconds`);
     },i);
 }
+*/
 
 
 /*
 let elem_1=prompt("Введите число :");
 let elem_2=prompt("Введите число :");
 
-mul(elem_1,elem_2);
+console.log(mul(elem_1,elem_2));
 
 function mul(n,m) {
-        console.log(n*m);
+        return n*m;
 }
 */
 
